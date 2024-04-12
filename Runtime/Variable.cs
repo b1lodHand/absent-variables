375139386367d4a90ae6bbc5fe84db84dbb520ae
absencee_ <84275165+b1lodHand@users.noreply.github.com>
2024-04-12 15:12:01 +0300

    added base Variable class.
    to be able to write custom editor.

--- Runtime/Variable.cs
+++ Runtime/Variable.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace com.absence.variablesystem
 {
-    public class Variable<T>
+    [System.Serializable]
+    public abstract class Variable
+    {
+
+    }
+
+    public abstract class Variable<T> : Variable
     {
         protected event Action<VariableValueChangedCallbackContext<T>> m_onValueChanged;
 
@@ -27,21 +35,182 @@ namespace com.absence.variablesystem
             }
         }
 
-        public void RegisterOnValueChangedEvent(Action<VariableValueChangedCallbackContext<T>> evt)
+        protected List<Mutation<T>> m_mutations  = new List<Mutation<T>>();
+
+        public void AddMutation(Mutation<T> mutation)
+        {
+            RevertMutations();
+            m_mutations.Add(mutation);
+            ApplyMutations();
+        }
+        public void RemoveMutation(Mutation<T> mutation)
+        {
+            RevertMutations();
+            if(m_mutations.Contains(mutation)) m_mutations.Remove(mutation);
+            ApplyMutations();
+        }
+        public void ClearMutators()
+        {
+            RevertMutations();
+            m_mutations.Clear();
+            ApplyMutations();
+        }
+
+        protected abstract void RevertMutations();
+        protected abstract void ApplyMutations();
+
+        public void AddValueChangeListener(Action<VariableValueChangedCallbackContext<T>> evt)
         {
             m_onValueChanged += evt;
         }
-
-        public void UnregisterOnValueChangedEvent(Action<VariableValueChangedCallbackContext<T>> evt)
+        public void RemoveValueChangeListener(Action<VariableValueChangedCallbackContext<T>> evt)
         {
             m_onValueChanged -= evt;
         }
     }
 
-    [System.Serializable] public class Variable_Integer : Variable<int> { }
-    [System.Serializable] public class Variable_Float : Variable<float> { }
-    [System.Serializable] public class Variable_String : Variable<string> { }
-    [System.Serializable] public class Variable_Boolean : Variable<bool> { }
+    [System.Serializable]
+    public class Variable_Integer : Variable<int>
+    {
+        protected override void RevertMutations()
+        {
+            if (m_mutations.Count == 0) return;
+
+            var value = Value;
+
+            var additives = m_mutations.Where(m => m.MutationType == MutationType.Additive).ToList();
+            var multiplicatives = m_mutations.Where(m => m.MutationType == MutationType.Multiplicative).ToList();
+
+            multiplicatives.ForEach(m =>
+            {
+                value /= m.MutationValue;
+            });
+
+            additives.ForEach(m =>
+            {
+                value -= m.MutationValue;
+            });
+
+            Value = value;
+        }
+
+        protected override void ApplyMutations()
+        {
+            if(m_mutations.Count == 0) return;
+
+            var value = Value;
+
+            var additives = m_mutations.Where(m => m.MutationType == MutationType.Additive).ToList();
+            var multiplicatives = m_mutations.Where(m => m.MutationType == MutationType.Multiplicative).ToList();
+
+            additives.ForEach(m =>
+            {
+                value += m.MutationValue;
+            });
+
+            multiplicatives.ForEach(m =>
+            {
+                value *= m.MutationValue;
+            });
+
+            Value = value;
+        }
+    }
+
+    [System.Serializable]
+    public class Variable_Float : Variable<float>
+    {
+        protected override void RevertMutations()
+        {
+            var value = Value;
+
+            var additives = m_mutations.Where(m => m.MutationType == MutationType.Additive).ToList();
+            var multiplicatives = m_mutations.Where(m => m.MutationType == MutationType.Multiplicative).ToList();
+
+            multiplicatives.ForEach(m =>
+            {
+                value /= m.MutationValue;
+            });
+
+            additives.ForEach(m =>
+            {
+                value -= m.MutationValue;
+            });
+
+            Value = value;
+        }
+
+        protected override void ApplyMutations()
+        {
+            var value = Value;
+
+            var additives = m_mutations.Where(m => m.MutationType == MutationType.Additive).ToList();
+            var multiplicatives = m_mutations.Where(m => m.MutationType == MutationType.Multiplicative).ToList();
+
+            additives.ForEach(m =>
+            {
+                value += m.MutationValue;
+            });
+
+            multiplicatives.ForEach(m =>
+            {
+                value *= m.MutationValue;
+            });
+
+            Value = value;
+        }
+    }
+
+    [System.Serializable]
+    public class Variable_Vector3
+    {
+        public Variable_Float X {  get; set; } = new Variable_Float();
+        public Variable_Float Y { get; set; } = new Variable_Float();
+        public Variable_Float Z { get; set; } = new Variable_Float();
+
+        public Vector3 GetValue() => new Vector3(X.Value, Y.Value, Z.Value);
+        public void SetValue(Vector3 newValue)
+        {
+            X.Value = newValue.x;
+            Y.Value = newValue.y;
+            Z.Value = newValue.z;
+        }
+
+        public void AddMutation(Mutation<float> mutation)
+        {
+            X.AddMutation(mutation);
+            Y.AddMutation(mutation);
+            Z.AddMutation(mutation);
+        }
+    }
+
+    [System.Serializable]
+    public class Variable_String : Variable<string>
+    {
+        protected override void ApplyMutations()
+        {
+            
+        }
+
+        protected override void RevertMutations()
+        {
+            
+        }
+    }
+
+    [System.Serializable]
+    public class Variable_Boolean : Variable<bool>
+    {
+        protected override void ApplyMutations()
+        {
+
+        }
+
+        protected override void RevertMutations()
+        {
+            
+        }
+    }
 
     public class VariableValueChangedCallbackContext<T>
     {
