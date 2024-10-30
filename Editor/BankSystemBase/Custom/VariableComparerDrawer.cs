b980312361c91bdc9ae3b8f07c7e0835bd263037
absencee_ <ilyasofli06@gmail.com>
2024-10-30 11:45:46 +0300

    Update VariableComparerDrawer.cs

--- Editor/BankSystemBase/Custom/VariableComparerDrawer.cs
+++ Editor/BankSystemBase/Custom/VariableComparerDrawer.cs
@@ -320,7 +320,17 @@ namespace com.absence.variablesystem.banksystembase.editor
             if (!comparer.HasFixedBank)
             {
                 var selectedBankIndex = EditorGUI.Popup(bankSelectorRect, VariableBankDatabase.Exists(currentBankGuid) ? VariableBankDatabase.GetIndexOf(currentBankGuid) : 0, VariableBankDatabase.GetBankNameList().ToArray());
+
+                if (selectedBankIndex < 0 || selectedBankIndex >= VariableBankDatabase.BanksInAssets.Count) 
+                    selectedBankIndex = 0;
+
                 targetBank = VariableBankDatabase.BanksInAssets[selectedBankIndex];
+                if (targetBank == null)
+                {
+                    bankGuidProp.stringValue = string.Empty;
+                    return;
+                }
+
                 bankGuidProp.stringValue = targetBank.Guid;
             }
 
